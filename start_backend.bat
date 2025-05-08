76dde5cc7aa5960bdeb8a7216e00c64cd26691d6
usertbera <134856746+usertbera@users.noreply.github.com>
2025-05-08 02:31:59 -0400

    Removed redundant files

--- start_backend.bat
+++
@@ -1,3 +0,0 @@
-cd enveye-backend
-uvicorn enveye_backend:app --host 0.0.0.0 --port 8000 --reload
-pause
--- start_frontend.bat
+++
@@ -1,5 +0,0 @@
-cd enveye-frontend
-npm install
-npm run build
-npm run preview -- --port 5173
-pause
